46e2745d924fc1a4f5ba1ae90dee391038373b3f
Tukayama <ya.tumanov2009@yandex.ru>
2021-09-12 18:21:58 +0300

    init commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int ticket = 32000;
+    // переменная a= 1 мили
+            int a = 20;
+            int miles = ticket / a;
+        System.out.println(miles);
+    }
+}
